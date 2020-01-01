609e7e0e001752c86ffe5442070b69a252ea4b8e
Asbin Rai <asbin.r@hotmail.co.uk>
2020-01-01 01:26:11 +0000

    Created a test for timetable and added sample route.csv files for both testing and the actual software

--- travelplanner/tests/test_travelplanner.py
+++ travelplanner/tests/test_travelplanner.py
@@ -2,6 +2,9 @@ from ..travelplanner import Passenger, Route, Journey
 import numpy as np
 import math
 import pytest
+from pathlib import Path
+
+DIR = Path(__file__).parent
 
 
 def test_walk_time():
@@ -9,3 +12,20 @@ def test_walk_time():
     expected = math.sqrt((1-5)**2 + (1-8)**2) * 10
     walk_time = p.walk_time()
     assert walk_time == pytest.approx(expected, 0.01)
+
+
+def test_timetable():
+
+    f = open(DIR / "route.csv", "w")
+    f.write("10,8,A\n10,9,\n10,10,\n10,11,\n9,11,\n8,11,\n7,11,\n7,10,\n7,9,\n6,9,\n5,9,\n4,9,\n3,9,\n2,9,\n2,8,\n2,7,\n2,6,B\n2,5,\n2,4,\n2,3,\n2,2,\n2,1,\n1,1,\n0,1,\n0,2,\n0,3,C\n")
+    f.close()
+
+    r = Route(DIR / "route.csv")
+    result = r.timetable()
+    expected = {'A': 0, 'B': 160, 'C': 250}
+    assert result == expected
+
+    r = Route(DIR / "route.csv", 20)
+    result = r.timetable()
+    expected = {'A': 0, 'B': 320, 'C': 500}
+    assert result == expected
