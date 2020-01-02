3afd52b6e959246c07218c97035e9486bef518c7
Asbin Rai <asbin.r@hotmail.co.uk>
2020-01-02 03:37:18 +0000

    Created fixtures in the file to remove some code repetition

--- travelplanner/tests/test_travelplanner.py
+++ travelplanner/tests/test_travelplanner.py
@@ -4,8 +4,22 @@ import math
 import pytest
 from pathlib import Path
 
-
 DIR = Path(__file__).parent
+
+
+@pytest.fixture
+def write_file():
+    f = open(DIR / "route.csv", "w")
+    f.write("10,8,A\n10,9,\n10,10,\n10,11,\n9,11,\n8,11,\n7,11,\n7,10,\n7,9,\n6,9,\n5,9,\n4,9,\n3,9,\n2,9,\n2,8,\n2,7,\n2,6,B\n2,5,\n2,4,\n2,3,\n2,2,\n2,1,\n1,1,\n0,1,\n0,2,\n0,3,C\n")
+    f.close()
+
+
+@pytest.fixture
+def passengers():
+    john = Passenger(start=(0, 2), end=(8, 1), speed=15)
+    mary = Passenger(start=(9, 8), end=(2, 4), speed=23)
+    connor = Passenger(start=(10, 8), end=(1, 2), speed=28)
+    return [john, mary, connor]
 
 
 def test_constructor_input():
@@ -47,10 +61,7 @@ def test_walk_time():
     assert walk_time == pytest.approx(expected, 0.01)
 
 
-def test_timetable():
-    f = open(DIR / "route.csv", "w")
-    f.write("10,8,A\n10,9,\n10,10,\n10,11,\n9,11,\n8,11,\n7,11,\n7,10,\n7,9,\n6,9,\n5,9,\n4,9,\n3,9,\n2,9,\n2,8,\n2,7,\n2,6,B\n2,5,\n2,4,\n2,3,\n2,2,\n2,1,\n1,1,\n0,1,\n0,2,\n0,3,C\n")
-    f.close()
+def test_timetable(write_file):
 
     # test without speed
     r = Route(DIR / "route.csv")
@@ -65,16 +76,10 @@ def test_timetable():
     assert result == expected
 
 
-def test_travel_time():
-    f = open(DIR / "route.csv", "w")
-    f.write("10,8,A\n10,9,\n10,10,\n10,11,\n9,11,\n8,11,\n7,11,\n7,10,\n7,9,\n6,9,\n5,9,\n4,9,\n3,9,\n2,9,\n2,8,\n2,7,\n2,6,B\n2,5,\n2,4,\n2,3,\n2,2,\n2,1,\n1,1,\n0,1,\n0,2,\n0,3,C\n")
-    f.close()
+def test_travel_time(write_file, passengers):
 
-    john = Passenger(start=(0, 2), end=(8, 1), speed=15)
-    mary = Passenger(start=(9, 8), end=(2, 4), speed=23)
-    connor = Passenger(start=(10, 8), end=(1, 2), speed=28)
     route = Route(DIR / "route.csv")
-    journey = Journey(route, [john, mary, connor])
+    journey = Journey(route, passengers)
 
     # test without speed
     id = 0
@@ -89,7 +94,7 @@ def test_travel_time():
     assert expected == results
 
     route = Route(DIR / "route.csv", 5)
-    journey = Journey(route, [john, mary, connor])
+    journey = Journey(route, passengers)
 
     # test with speed
     id = 0
@@ -104,16 +109,10 @@ def test_travel_time():
     assert expected == results
 
 
-def test_print_time_stats(capsys):
-    f = open(DIR / "route.csv", "w")
-    f.write("10,8,A\n10,9,\n10,10,\n10,11,\n9,11,\n8,11,\n7,11,\n7,10,\n7,9,\n6,9,\n5,9,\n4,9,\n3,9,\n2,9,\n2,8,\n2,7,\n2,6,B\n2,5,\n2,4,\n2,3,\n2,2,\n2,1,\n1,1,\n0,1,\n0,2,\n0,3,C\n")
-    f.close()
+def test_print_time_stats(capsys, write_file, passengers):
 
-    john = Passenger(start=(0, 2), end=(8, 1), speed=15)
-    mary = Passenger(start=(9, 8), end=(2, 4), speed=23)
-    connor = Passenger(start=(10, 8), end=(1, 2), speed=28)
     route = Route(DIR / "route.csv")
-    journey = Journey(route, [john, mary, connor])
+    journey = Journey(route, passengers)
 
     # test without speed
     journey.print_time_stats()
@@ -123,7 +122,7 @@ def test_print_time_stats(capsys):
     assert result.out == expected
 
     route = Route(DIR / "route.csv", 5)
-    journey = Journey(route, [john, mary, connor])
+    journey = Journey(route, passengers)
 
     # test with speed
     journey.print_time_stats()
@@ -134,10 +133,7 @@ def test_print_time_stats(capsys):
     assert result.out == expected
 
 
-def test_generate_cc(capsys):
-    f = open(DIR / "route.csv", "w")
-    f.write("10,8,A\n10,9,\n10,10,\n10,11,\n9,11,\n8,11,\n7,11,\n7,10,\n7,9,\n6,9,\n5,9,\n4,9,\n3,9,\n2,9,\n2,8,\n2,7,\n2,6,B\n2,5,\n2,4,\n2,3,\n2,2,\n2,1,\n1,1,\n0,1,\n0,2,\n0,3,C\n")
-    f.close()
+def test_generate_cc(capsys, write_file):
 
     # test without speed
     route = Route(DIR / "route.csv")
