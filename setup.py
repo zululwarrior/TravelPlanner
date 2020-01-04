1638b93f3416be433f0fb8c875d00d1a3d887a34
Asbin Rai <asbin.r@hotmail.co.uk>
2020-01-04 20:51:55 +0000

    Added author name to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="travelplanner",
     version="1.0.0",
-    packages=find_packages(exclude=['*test']),
+    author="Asbin Rai",
+    packages=find_packages(exclude=['*test*']),
     install_requires=['numpy', 'matplotlib'],
     entry_points={
         'console_scripts': [
